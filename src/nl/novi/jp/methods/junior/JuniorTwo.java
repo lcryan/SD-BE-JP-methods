b4599f779c9305cf1ebc164adccffe710c3548b4
LuciaRyan <lcryan06@gmail.com>
2023-05-16 15:58:22 +0200

    Junior Two solved

--- src/nl/novi/jp/methods/junior/JuniorTwo.java
+++ src/nl/novi/jp/methods/junior/JuniorTwo.java
@@ -4,7 +4,6 @@ package nl.novi.jp.methods.junior;
 /**
  * Deze klasse bevat al de methode met de naam doubleIt. Deze methode ontvangt een integer, verdubbelt deze en print
  * deze vervolgens uit.
- *
  * Maak deze klasse af door de methode squareTheNumber af te maken. Deze methode ontvangt een integer en moet deze
  * kwadrateren en uitprinten.
  */
@@ -12,7 +11,7 @@ public class JuniorTwo {
 
     public static void main(String[] args) {
         doubleIt(33);
-        squareTheNumber();
+        squareTheNumber(25);
     }
 
     public static void doubleIt(int number) {
@@ -20,7 +19,8 @@ public class JuniorTwo {
         System.out.println("Het getal " + number + " is verdubbeld: " + doubledNumber);
     }
 
-    public static void squareTheNumber() {
-
+    public static void squareTheNumber(int number) {
+        int squareOfNumber = number * number;
+        System.out.println(squareOfNumber);
     }
 }
