1b81eb7cb8afc471929838be0a6e800e98a8fde2
LuciaRyan <lcryan06@gmail.com>
2023-05-16 16:00:04 +0200

    Junior Three solved

--- src/nl/novi/jp/methods/junior/JuniorThree.java
+++ src/nl/novi/jp/methods/junior/JuniorThree.java
@@ -2,13 +2,13 @@ package nl.novi.jp.methods.junior;
 
 /**
  * Deze klasse bevat al de methode negativeNumberChecker. Beantwoord voor jezelf wat deze methode doet.
- *
+ * // this method checks on if a number is negative or not
  * Roep de methode een paar keer aan om zijn gedrag in de praktijk te zien.
  */
 
 public class JuniorThree {
     public static void main(String[] args) {
-
+negativeNumberChecker(-100);
     }
 
     public static void negativeNumberChecker(int number) {
