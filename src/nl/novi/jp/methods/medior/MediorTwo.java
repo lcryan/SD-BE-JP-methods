3cb17bcfebb13eb3cb2e3f6aa91be3c39daabc27
LuciaRyan <lcryan06@gmail.com>
2023-05-18 16:34:43 +0200

    Medior Two solved

--- src/nl/novi/jp/methods/medior/MediorTwo.java
+++ src/nl/novi/jp/methods/medior/MediorTwo.java
@@ -2,22 +2,22 @@ package nl.novi.jp.methods.medior;
 
 /**
  * Gegeven: Een methode die true of false (boolean) teruggeeft op basis van de gegeven leeftijd.
- *
+ * <p>
  * Roep deze methode (is oldEnough) vanuit de main-methode aan. Op basis van de return value, roep je
  * of printUnderAgeWarning() of printProceed() uit.
  */
 public class MediorTwo {
 
     public static void main(String[] args) {
-
+       if(isOldEnough(41)) { // don't forget to fill in an argument here !
+           printProceed();
+       } else {
+           printUnderAgeWarning();
+       }
     }
 
-    public  static boolean isOldEnough(int age) {
-        if(age > 17) {
-            return true;
-        } else {
-            return false;
-        }
+    public static boolean isOldEnough(int age) {
+        return age > 17;
     }
 
     public static void printUnderAgeWarning() {
