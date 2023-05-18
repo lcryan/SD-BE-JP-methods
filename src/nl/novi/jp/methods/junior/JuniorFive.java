e43ce4a1a84a32944f9f084eb03de35b7b923a1c
LuciaRyan <lcryan06@gmail.com>
2023-05-18 16:17:27 +0200

    Medior One Solved Medior Five - something off with the Java File

--- src/nl/novi/jp/methods/junior/JuniorFive.java
+++ src/nl/novi/jp/methods/junior/JuniorFive.java
@@ -1,18 +1,18 @@
 package nl.novi.jp.methods.junior;
 
 
-
 /**
  * Deze klasse is helemaal leeg, je mag nu dus alles zelf doen.
  * Maak een methode die een String ontvangt en deze in HOOFDLETTERS uitprint.
  * Bedenk zelf een goede Engelse naam voor de methode.
  * <p>
  * Hoe maak je hoofdletters van een String in Java?
-/* * https://duckduckgo.com/?q=java+to+uppercase
- */
+**/
 
 public class JuniorFive {
     public void main(String[] args) {
 
+        rockbandsInUpperCase("Muse");
     }
-}
+    public static void rockbandsInUpperCase(String bandName) {
+        System.out.println("bandName = " + bandName);}}
--- src/nl/novi/jp/methods/medior/MediorOne.java
+++ src/nl/novi/jp/methods/medior/MediorOne.java
@@ -3,25 +3,31 @@ package nl.novi.jp.methods.medior;
 /**
  * De methode calculateSum returnt een waarde. Deze waarde kun je toewijzen aan een nieuwe variabele en andere
  * berekeningen of bewerkingen mee doen. In het voorbeeld hieronder wordt de waarde aan de variabele result toegewezen.
- *
+ * <p>
  * Zou je de waarde niet returnen, maar direct printen, dan kun je er niets meer mee doen. Nu wel, we kunnen het
  * getal in een andere methode gooien en daar weer bewerkingen mee doen.
- *
+ * <p>
  * Kun je een methode schrijven die het getal kwadrateert en deze returnt als integer?
- *
+ * <p>
  * Roep deze vervolgens aan vanuit de main-methode.
  */
 
 public class MediorOne {
 
     public static void main(String[] args) {
-        int result = calculateSum(10, 22);
+
+        int result = calculateSum(10, 12);
 
         System.out.println("De som van 10 en 12 is: " + result);
 
         int doubledResult = doubleNumber(result);
 
         System.out.println(result + " verdubbeld is: " + doubledResult);
+
+
+        int squaredResult = squareNumber(doubledResult);
+
+        System.out.println("The square of " + doubledResult + " is " + squaredResult);
     }
 
     public static int calculateSum(int firstNumber, int secondNumber) {
@@ -32,5 +38,9 @@ public class MediorOne {
         return number * 2;
     }
 
-
+    public static int squareNumber(int number) {
+         return number * number;
+    }
 }
+
+
